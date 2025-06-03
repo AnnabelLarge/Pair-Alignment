10f79790070e7df18134c8f81c6e866e01c82d6b
AnnabelLarge <annabel_large@berkeley.edu>
2025-06-03 15:29:08 -0700

    new option to pre-load dataloader list

--- Pair_Alignment.py
+++ Pair_Alignment.py
@@ -34,11 +34,11 @@ def main():
     
     ### which program do you want to run?
     valid_tasks = ['train',
-                    'eval',
-                    'batched_train',
-                    'batched_eval',
-                    # 'label_class_post',
-                    'continue_train']
+                   'batched_train',
+                   'continue_train',
+                   'eval',
+                   'batched_eval',
+                   'prep_dataloaders']
     
     parser.add_argument('-task',
                         type=str,
@@ -51,29 +51,36 @@ def main():
                         required=True,
                         help='Load configs from file or folder of files, in json format.')
     
+    # optional: might have pre-processed some dataloaders
+    parser.add_argument('-load_dload_pkl', 
+                        action='store_true', 
+                        help='If added, load dataloaders from a pickle object (it was already created and pre-processed beforehand)')
+    
     # only needed when continuing training
     parser.add_argument('-new_training_wkdir',
                         type = str,
-                        help='ONLY FOR CONTINUE_TRAIN OPTION; Name for a new training working dir')
+                        help='FOR CONTINUE_TRAIN OPTION; Name for a new training working dir')
     
     parser.add_argument('-prev_model_ckpts_dir',
                         type = str,
-                        help='ONLY FOR CONTINUE_TRAIN OPTION; Path to previous trainstate, argparse object')
+                        help='FOR CONTINUE_TRAIN OPTION; Path to previous trainstate, argparse object')
     
     parser.add_argument('-tstate_to_load',
                         type = str,
-                        help='ONLY FOR CONTINUE_TRAIN OPTION; The name of the tstate object to load')
+                        help='FOR CONTINUE_TRAIN OPTION; The name of the tstate object to load')
     
     # parse the arguments
-    args = parser.parse_args()
+    top_level_args = parser.parse_args()
     
     
     # ## UNCOMMENT TO RUN IN SPYDER IDE
-    # args.task = 'train'
-    # args.configs = 'example_config_fragment_site_class_model.json'
+    # top_level_args.task = 'train'
+    # top_level_args.configs = 'example_config_indp_sites_model.json'
+    # top_level_args.load_dload_pkl = False
     
     
-    ### helper function to open a single config file and extract additional arguments
+    ### helper functions 
+    # open a single config file and extract additional arguments
     def read_config_file(config_file):
         with open(config_file, 'r') as f:
             contents = json.load(f)
@@ -81,16 +88,23 @@ def main():
             t_args.__dict__.update(contents)
             args = parser.parse_args(namespace=t_args)
         return args
+    
+    # load a pre-computed dataloader
+    def load_dload_pkl(training_wkdir):
+        file_to_load = f'TMP-dload-lst_' + training_wkdir.replace('.json','.pkl')
+        with open(file_to_load,'rb') as f:
+            dload_lst = pickle.load(f)
+        return dload_lst
 
 
     ###########################################################################
     ### TRAINING   ############################################################
     ###########################################################################
-    if args.task == 'train':
+    if top_level_args.task == 'train':
         # read argparse
-        assert args.configs.endswith('.json'), print("input is one JSON file")
-        print(f'TRAINING WITH: {args.configs}')
-        args = read_config_file(args.configs)
+        assert top_level_args.configs.endswith('.json'), print("input is one JSON file")
+        print(f'TRAINING WITH: {top_level_args.configs}')
+        args = read_config_file(top_level_args.configs)
         
         # import correct wrappers, dataloader initializers
         if args.pred_model_type == 'pairhmm_indp_sites':
@@ -111,27 +125,32 @@ def main():
             from cli.train_feedforward import train_feedforward as train_fn
             from dloaders.init_full_len_dset import init_full_len_dset as init_dataloaders
 
+        # make dataloder list
+        if not top_level_args.load_dload_pkl:
+            dload_lst = init_dataloaders( args,
+                                          'train',
+                                          training_argparse = None )
+        elif top_level_args.load_dload_pkl:
+            dload_lst = load_dload_pkl(training_wkdir = args.training_wkdir)
+            
         # train model
-        dload_lst = init_dataloaders( args,
-                                      'train',
-                                      training_argparse = None )
-        
         train_fn( args, dload_lst )
 
-    elif args.task == 'batched_train':
+
+    elif top_level_args.task == 'batched_train':
         ### read argparse from first config file
-        file_lst = [file for file in os.listdir(args.configs) if not file.startswith('.')
+        file_lst = [file for file in os.listdir(top_level_args.configs) if not file.startswith('.')
                     and file.endswith('.json')]
         assert len(file_lst) > 0, f'{args.configs} is empty!'
         
         
         ### get dataloader and functions from first config file
         first_config_file = file_lst[0]
-        print(f'DATALOADER CONSTRUCTED FROM: {args.configs}/{first_config_file}')
-        print(f"WARNING: make sure you want this dataloader for ALL experiments in {args.configs}!!!")
-        print(f"WARNING: make sure you want this dataloader for ALL experiments in {args.configs}!!!")
-        print(f"WARNING: make sure you want this dataloader for ALL experiments in {args.configs}!!!")
-        first_args = read_config_file(f'{args.configs}/{first_config_file}')
+        print(f'DATALOADER CONSTRUCTED FROM: {top_level_args.configs}/{first_config_file}')
+        print(f"WARNING: make sure you want this dataloader for ALL experiments in {top_level_args.configs}!!!")
+        print(f"WARNING: make sure you want this dataloader for ALL experiments in {top_level_args.configs}!!!")
+        print(f"WARNING: make sure you want this dataloader for ALL experiments in {top_level_args.configs}!!!")
+        first_args = read_config_file(f'{top_level_args.configs}/{first_config_file}')
         
         # import correct wrappers, dataloader initializers
         if first_args.pred_model_type == 'pairhmm_indp_sites':
@@ -152,31 +171,30 @@ def main():
             from cli.train_feedforward import train_feedforward as train_fn
             from dloaders.init_full_len_dset import init_full_len_dset as init_dataloaders
 
-        # load data
-        dload_lst_for_all = init_dataloaders( first_args, 
-                                             'train',
-                                             training_argparse = None  )
-        
-        del first_args
-        
-        
+        # load data; due to batched nature, no option to pre-compute (for now? 
+        #   if I need it later, make it happen)
+        dload_lst_for_all = init_dataloaders( first_args,
+                                              'train',
+                                              training_argparse = None )
+            
         ### with this dload_lst, train using ALL config files
         for file in file_lst:
             # read argparse
             assert file.endswith('.json'), print("input is one JSON file")
-            this_run_args = read_config_file(f'{args.configs}/{file}')
-            print(f'TRAINING WITH: {args.configs}/{file}')
+            this_run_args = read_config_file(f'{top_level_args.configs}/{file}')
+            print(f'TRAINING WITH: {top_level_args.configs}/{file}')
             
             train_fn( this_run_args, 
                       dload_lst_for_all )
             
             del this_run_args
     
-    elif args.task == 'continue_train':
+    
+    elif top_level_args.task == 'continue_train':
         # read argparse
-        assert args.configs.endswith('.json'), print("input is one JSON file")
-        print(f'CONTINUE TRAINING WITH: {args.configs}, IN NEW DIR {args.new_training_wkdir}')
-        args_from_training_config = read_config_file(args.configs)
+        assert top_level_args.configs.endswith('.json'), print("input is one JSON file")
+        print(f'CONTINUE TRAINING WITH: {top_level_args.configs}, IN NEW DIR {top_level_args.new_training_wkdir}')
+        args_from_training_config = read_config_file(top_level_args.configs)
         
         # import correct wrappers, dataloader initializers
         if args_from_training_config.pred_model_type == 'pairhmm_indp_sites':
@@ -185,29 +203,32 @@ def main():
         
         else:
             raise NotImplementedError('Cannot continue training yet!')
-        
 
+        # make dataloader objects
+        if not top_level_args.load_dload_pkl:
+            dload_lst = init_dataloaders( args_from_training_config,
+                                          'train',
+                                          training_argparse = None )
+        elif top_level_args.load_dload_pkl:
+            dload_lst = load_dload_pkl(training_wkdir = args_from_training_config.training_wkdir)
+            
         # train model
-        dload_lst = init_dataloaders( args_from_training_config,
-                                      'train',
-                                      training_argparse = None )
-        
         cont_train_fn( args=args_from_training_config, 
-                       dataloader_dict=dload_lst,
-                       new_training_wkdir=args.new_training_wkdir,
-                       prev_model_ckpts_dir=args.prev_model_ckpts_dir,
-                       tstate_to_load=args.tstate_to_load
-                       )
-      
+                        dataloader_dict=dload_lst,
+                        new_training_wkdir=args.new_training_wkdir,
+                        prev_model_ckpts_dir=args.prev_model_ckpts_dir,
+                        tstate_to_load=args.tstate_to_load
+                        )
+    
     
     ###########################################################################
     ### EVAL   ################################################################
     ###########################################################################
-    elif args.task == 'eval':
+    elif top_level_args.task == 'eval':
         ### read argparse
-        assert args.configs.endswith('.json'), print("input is one JSON file")
-        print(f'EVALUATING WITH: {args.configs}')
-        args = read_config_file(args.configs)
+        assert top_level_args.configs.endswith('.json'), print("input is one JSON file")
+        print(f'EVALUATING WITH: {top_level_args.configs}')
+        args = read_config_file(top_level_args.configs)
         
         
         ### find and read training argparse
@@ -226,34 +247,37 @@ def main():
             from dloaders.init_counts_dset import init_counts_dset as init_dataloaders
 
         elif pred_model_type == 'pairhmm_frag_and_site_classes':
-            raise NotImplementedError('not ready')
             from cli.eval_pairhmm_frag_and_site_classes import eval_pairhmm_frag_and_site_classes as eval_fn
             from dloaders.init_full_len_dset import init_full_len_dset as init_dataloaders
 
+        # load data; saved under trianing_wkdir name
+        if not top_level_args.load_dload_pkl:
+            dload_lst = init_dataloaders( args,
+                                          'eval',
+                                          training_argparse )
+        elif top_level_args.load_dload_pkl:
+            dload_lst = load_dload_pkl(training_wkdir = args.training_wkdir)
+            
         # evaluate model
-        dload_lst = init_dataloaders( args, 
-                                      'eval',
-                                      training_argparse )
-        
         eval_fn( args, 
-                 dload_lst, 
-                 training_argparse )
+                  dload_lst, 
+                  training_argparse )
     
     
-    elif args.task == 'batched_eval':
+    elif top_level_args.task == 'batched_eval':
         ### read argparse from first config file
-        file_lst = [file for file in os.listdir(args.configs) if not file.startswith('.')
+        file_lst = [file for file in os.listdir(top_level_args.configs) if not file.startswith('.')
                     and file.endswith('.json')]
-        assert len(file_lst) > 0, f'{args.configs} is empty!'
+        assert len(file_lst) > 0, f'{top_level_args.configs} is empty!'
         
         
         ### get dataloader and functions from first config file
         first_config_file = file_lst[0]
-        print(f'DATALOADER CONSTRUCTED FROM: {args.configs}/{first_config_file}')
-        print(f"WARNING: make sure you want this dataloader for ALL experiments in {args.configs}!!!")
-        print(f"WARNING: make sure you want this dataloader for ALL experiments in {args.configs}!!!")
-        print(f"WARNING: make sure you want this dataloader for ALL experiments in {args.configs}!!!")
-        first_args = read_config_file(f'{args.configs}/{first_config_file}')
+        print(f'DATALOADER CONSTRUCTED FROM: {top_level_args.configs}/{first_config_file}')
+        print(f"WARNING: make sure you want this dataloader for ALL experiments in {top_level_args.configs}!!!")
+        print(f"WARNING: make sure you want this dataloader for ALL experiments in {top_level_args.configs}!!!")
+        print(f"WARNING: make sure you want this dataloader for ALL experiments in {top_level_args.configs}!!!")
+        first_args = read_config_file(f'{top_level_args.configs}/{first_config_file}')
         
         # find and read training argparse
         first_training_argparse_filename = (f'{os.getcwd()}/'+
@@ -288,8 +312,8 @@ def main():
         for file in file_lst:
             # read argparse
             assert file.endswith('.json'), print("input is one JSON file")
-            this_run_args = read_config_file(f'{args.configs}/{file}')
-            print(f'EVALUATING WITH: {args.configs}/{file}')
+            this_run_args = read_config_file(f'{top_level_args.configs}/{file}')
+            print(f'EVALUATING WITH: {top_level_args.configs}/{file}')
             
             # find and read training argparse
             training_argparse_filename = (f'{os.getcwd()}/'+
@@ -310,12 +334,12 @@ def main():
     ###########################################################################
     ### EVAL: label class posterior marginals for markovian class sites   #####
     ###########################################################################
-    elif args.task == 'label_class_post':
+    elif top_level_args.task == 'label_class_post':
         raise NotImplementedError('not ready yet')
         ### read argparse
-        assert args.configs.endswith('.json'), print("input is one JSON file")
-        print(f'EVALUATING WITH: {args.configs}')
-        args = read_config_file(args.configs)
+        assert top_level_args.configs.endswith('.json'), print("input is one JSON file")
+        print(f'EVALUATING WITH: {top_level_args.configs}')
+        args = read_config_file(top_level_args.configs)
         
         
         ### find and read training argparse
@@ -332,14 +356,70 @@ def main():
         from cli.class_posteriors_pairhmm_frag_and_site_classes import class_posteriors_pairhmm_frag_and_site_classes as labeling_fn
         from dloaders.init_full_len_dset import init_full_len_dset as init_dataloaders
 
-        # evaluate
-        dload_lst = init_dataloaders( args, 
-                                      'eval',
-                                      training_argparse )
+        # load data (saved under training_wkdir name)
+        if not top_level_args.load_dload_pkl:
+            dload_lst = init_dataloaders( args,
+                                          'eval',
+                                          training_argparse )
+        elif top_level_args.load_dload_pkl:
+            dload_lst = load_dload_pkl(training_wkdir = args.training_wkdir)
         
+        # evaluate
         labeling_fn( args, 
                      dload_lst, 
                      training_argparse )
+    
+    
+    ###########################################################################
+    ### MISC: prepare dataloaders for downstream use   ########################
+    ###########################################################################
+    elif top_level_args.task == 'prep_dataloaders':
+        file_lst = [file for file in os.listdir(top_level_args.configs) if not file.startswith('.')
+                    and file.endswith('.json')]
+        assert len(file_lst) > 0, f'{top_level_args.configs} is empty!'
+        
+        
+        ### get modules from first config file
+        first_config_file = file_lst[0]
+        first_args = read_config_file(f'{top_level_args.configs}/{first_config_file}')
+        
+        # import correct wrappers, dataloader initializers
+        if first_args.pred_model_type == 'pairhmm_indp_sites':
+            from cli.train_pairhmm_indp_sites import train_pairhmm_indp_sites as train_fn
+            from dloaders.init_counts_dset import init_counts_dset as init_dataloaders
+
+        elif first_args.pred_model_type in ['pairhmm_frag_and_site_classes',
+                                            'neural_hmm',
+                                            'feedforward']:
+            from cli.train_pairhmm_frag_and_site_classes import train_pairhmm_frag_and_site_classes as train_fn
+            from dloaders.init_full_len_dset import init_full_len_dset as init_dataloaders
+            
+        del first_config_file, first_args
+        
+        
+        ### read every file, then export a raw pickle 
+        for file in file_lst:
+            # read argparse
+            assert file.endswith('.json'), print("input is one JSON file")
+            this_run_args = read_config_file(f'{top_level_args.configs}/{file}')
+            print(f'LOADING FROM: {top_level_args.configs}/{file}')
+            
+            # determine if four or two dataloaders should be returned
+            checklist = ['train_dset_splits', 'test_dset_splits', 'optimizer_config']
+            train_flag = all(arg in dir(args) for arg in checklist)
+            
+            # load
+            dload_lst = init_dataloaders( this_run_args, 
+                                          'train' if train_flag else 'eval',
+                                          training_argparse = None )
+            
+            # dump
+            new_file_name = f'TMP-dload-lst_' + file.replace('.json','.pkl')
+            print(f'SAVING TO: {new_file_name}')
+            with open(new_file_name, 'wb') as g:
+                pickle.dump(dload_lst, g)
+        
+        print('done')
         
     
 if __name__ == '__main__':
